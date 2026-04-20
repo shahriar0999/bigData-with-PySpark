4350d9c1d51a87151396d5c76295d09576396d08
Shahriar Kabir <110348315+shahriar0999@users.noreply.github.com>
2026-04-21 05:11:24 +0600

    Add CountingBykeys.py to count unique keys
    Implement key counting using countByKey in PySpark.

---
+++ big-data-fundamentals-with-pyspark/Programming in PySpark RDD’s/CountingBykeys.py
@@ -0,0 +1,9 @@
+# Count the unique keys
+total = Rdd.countByKey()
+
+# What is the type of total?
+print("The type of total is", type(total))
+
+# Iterate over the total and print the output
+for k, v in total.items(): 
+  print("key", k, "has", v, "counts")
