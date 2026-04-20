48b0139b4ab3d114786e8116f024fc10e2e604ee
Shahriar Kabir <110348315+shahriar0999@users.noreply.github.com>
2026-04-21 05:23:16 +0600

    Add functionality to remove stop words and count words

---
+++ big-data-fundamentals-with-pyspark/Programming in PySpark RDD’s/remove_stop_words_and_reduce_the_dataset.py
@@ -0,0 +1,8 @@
+# Filter splitRDD to remove stop words from the stop_words curated list
+splitRDD_no_stop = splitRDD_no_stop = splitRDD.filter(lambda x: x.lower() not in stop_words)
+
+# Create a tuple of the word (w) and 1 
+splitRDD_no_stop_words = splitRDD_no_stop.map(lambda w: (w,1))
+
+# Count of the number of occurences of each word
+resultRDD = splitRDD_no_stop_words.reduceByKey(lambda x, y: x + y)
